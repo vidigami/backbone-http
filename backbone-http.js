88598fdb668e38ef2b1df01bae8355c5e27f631c
Kevin Malakoff <xmann.intl@gmail.com>
2013-10-31 11:54:12 -0700

    Browserified tests

--- backbone-http.js
+++ backbone-http.js
@@ -199,6 +199,8 @@ module.exports = HTTPCursor = (function(_super) {
   Dependencies: Backbone.js, Underscore.js, Moment.js, Inflection.js, BackboneORM, and Superagent.
 */
 
+var bborm, path, _i, _len, _ref;
+
 if ((typeof window !== "undefined" && window !== null) && require.shim) {
   require.shim([
     {
@@ -213,19 +215,39 @@ if ((typeof window !== "undefined" && window !== null) && require.shim) {
       symbol: 'Backbone',
       path: 'backbone'
     }, {
-      symbol: 'superagent',
-      path: 'superagent'
+      symbol: 'moment',
+      path: 'moment'
+    }, {
+      symbol: 'inflection',
+      path: 'inflection'
+    }, {
+      symbol: 'stream',
+      path: 'stream',
+      optional: true
     }, {
       symbol: 'Backbone.ORM',
       symbol_path: 'backbone.ORM',
       path: 'backbone-orm'
+    }, {
+      symbol: 'superagent',
+      path: 'superagent'
     }
   ]);
 }
 
 module.exports = {
-  sync: require('./sync')
+  sync: require('./sync'),
+  modules: {
+    'backbone-orm': bborm = require('backbone-orm'),
+    'superagent': require('superagent')
+  }
 };
+
+_ref = ['url', 'querystring', 'lru-cache', 'underscore', 'backbone', 'moment', 'inflection', 'stream'];
+for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+  path = _ref[_i];
+  module.exports.modules[path] = bborm.modules[path];
+}
 
 });
 
--- src/index.coffee
+++ src/index.coffee
@@ -10,9 +10,20 @@ if window? and require.shim
   require.shim([
     {symbol: '_', path: 'lodash', alias: 'underscore', optional: true}, {symbol: '_', path: 'underscore'}
     {symbol: 'Backbone', path: 'backbone'}
-    {symbol: 'superagent', path: 'superagent'}
+    {symbol: 'moment', path: 'moment'}
+    {symbol: 'inflection', path: 'inflection'}
+    {symbol: 'stream', path: 'stream', optional: true} # stream is large so it is optional on the client
     {symbol: 'Backbone.ORM', symbol_path: 'backbone.ORM', path: 'backbone-orm'}
+    {symbol: 'superagent', path: 'superagent'}
   ])
 
 module.exports =
   sync: require './sync'
+
+  # re-expose modules
+  modules:
+    'backbone-orm': bborm = require 'backbone-orm'
+    'superagent': require 'superagent'
+
+for path in ['url', 'querystring', 'lru-cache', 'underscore', 'backbone', 'moment', 'inflection', 'stream']
+  module.exports.modules[path] = bborm.modules[path]
